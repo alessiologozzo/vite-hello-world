eedc4a0f093f4076666b5c4ef4286a32bd14570f
alessiologozzo <alessio.logozzo@libero.it>
2023-04-13 01:03:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader/>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default{
+  name: "App",
+  components:{
+    AppHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+export default{
+    name: "AppHeader",
+    data(){
+        return{
+            title: "La mia prima app Vite!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
